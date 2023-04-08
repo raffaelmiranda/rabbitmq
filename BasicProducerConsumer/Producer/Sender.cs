faf9c92e20fa44fc7bb183f2dc9c38063bfd14fa
Rafael Miranda <rafael.miranda@solutis.com.br>
2023-04-08 18:31:43 -0300

    commit

---
+++ BasicProducerConsumer/Producer/Sender.cs
@@ -0,0 +1,21 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+
+var factory = new ConnectionFactory() { HostName = "localhost" };
+using (var connection = factory.CreateConnection())
+using (var channel = connection.CreateModel())
+{
+    channel.QueueDeclare("BasicTest", false, false, false, null);
+
+    const string message = "Getting started with .Net Core RabbitMQ";
+
+    var body = Encoding.UTF8.GetBytes(message);
+
+    channel.BasicPublish("", "BasicTest", null, body);
+
+    Console.WriteLine("Sent message {0}...", message);
+}
+
+Console.WriteLine("Press [enter] to exit the Send App...");
+Console.ReadLine();
+

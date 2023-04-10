993ee97d7c287b8d7d531384921bd4a0d6f3a047
Rafael Miranda <rafael.miranda@solutis.com.br>
2023-04-10 07:39:39 -0300

    commit

---
+++ MicroRabbit/MicroRabbit.Mvc/Services/TransferService.cs
@@ -0,0 +1,24 @@
+﻿using MicroRabbit.MVC.Models.DTO;
+using System.Text.Json;
+
+namespace MicroRabbit.MVC.Services
+{
+    public class TransferService : ITransferService
+    {
+        private readonly HttpClient _apiClient;
+
+        public TransferService(HttpClient apiClient)
+        {
+            _apiClient = apiClient;
+        }
+
+        public async Task Transfer(TransferDto transferDto)
+        {
+            var uri = "https://localhost:7275/api/Banking";
+            var transferContent = new StringContent(JsonSerializer.Serialize(transferDto),
+                                            System.Text.Encoding.UTF8, "application/json");
+            var response = await _apiClient.PostAsync(uri, transferContent);
+            response.EnsureSuccessStatusCode();
+        }
+    }
+}
